a3add4eed4c451258638ebc59749369f978f5d6e
salam6919 <usazhaan@icloud.com>
2024-01-24 14:39:58 -0600

    progress

--- TicketMaster.java
+++ TicketMaster.java
@@ -1,4 +1,7 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class TicketMaster {
     private ArrayList<Show> shows;
@@ -6,7 +9,24 @@ public class TicketMaster {
     public TicketMaster(ArrayList<Show> shows){
         this.shows = shows;
     }
-
+    public void loadData(){
+        File rawShows = new File("showData.txt");
+        try {
+            Scanner fscanner = new Scanner(rawShows);
+            while (fscanner.hasNextLine()) {
+                String line = fscanner.nextLine();
+                String[] split_line = line.split(", ");
+                String city = split_line[1];
+                String[] otherData = split_line[0].split(" ");
+                Show show = new Show(otherData[0], otherData[1], otherData[2], otherData[3], city);
+                shows.add(show);
+            }
+            fscanner.close();
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
     public void sortByPrice(){
         for(int i = 1; i < shows.size(); i++){
             int j = i-1;
@@ -16,16 +36,30 @@ public class TicketMaster {
             }
         }
     }
+    public void sortByPerformer(){
+        for(int i = 0; i < shows.size(); i++){
+            int minimumIndex = i;
+            for(int j = i; j < shows.size(); j++){
+                if(shows.get(j) < shows.get(minimumIndex)){
+                    minimumIndex = j;
+                }
+            }
+            swap(shows, minimumIndex, i);
+        }
+    }
     private ArrayList<Show> swap(ArrayList<Show> shows, int x, int y){
         Show z = shows.get(x);
         shows.set(x, shows.get(y));
         shows.set(y, z);
         return shows;
     }
-    public void sortByPerformer(){
-        return;
-    }
     public ArrayList<Show> searchByCity(String city){
-
+        ArrayList<Show> searchResults = new ArrayList<>();
+        for(Show item: shows){
+            if(item.getCity().equals(city)){
+                searchResults.add(item);
+            }
+        }
+        return searchResults;
     }
 }
--- TicketMasterDriver.java
+++ TicketMasterDriver.java
@@ -1,2 +1,23 @@
+import java.util.Scanner;
+
 public class TicketMasterDriver {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\t*** Welcome to the Ticket Master Kiosk ***");
+        System.out.println("You may search our shows by city as well as by performer and by ticket price");
+        System.out.println("\tSimply select the correct option corresponding with your choice");
+        String userInput = scanner.nextLine();
+        while(!isNumeric(userInput) || Integer.parseInt(userInput) > 6){
+            System.out.println("Invalid entry. Please provide an integer 1-6");
+        }
+        while()
+    }
+    public static boolean isNumeric(String str) {
+        try {
+            Integer.parseInt(str);
+            return true;
+        } catch(NumberFormatException e){
+            return false;
+        }
+    }
 }
